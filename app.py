cffceaa0a5d0fb83f1d7482fdd5d1a6d828dc763
host <host>
2019-05-26 19:39:51 +0000

    anda

---
+++ app.py
@@ -0,0 +1,34 @@
+import pymysql
+from flask import Flask, request, render_template, redirect
+
+db = pymysql.connect("arishkage.mysql.pythonanywhere-services.com", "arishkage", "15101989python", "arishkage$url")
+#pone el cursor en la DB
+cursor = db.cursor()
+
+base_url = 'http://arishkage.pythonanywhere.com/'
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        url = request.form['url']
+
+        sql = "INSERT INTO url(ID,URL) VALUES (ID, '{}')".format( url)
+        cursor.execute(sql)
+        db.commit()
+
+        short_url = cursor.lastrowid
+        return render_template('index.html', short_url=base_url + str(short_url))
+    return render_template('index.html')
+
+@app.route('/<short_url>')
+def redirect_url(short_url):
+    sql = "select url from url where id = {}".format(short_url)
+    cursor.execute(sql)
+    url = cursor.fetchone()[0]
+
+    return redirect(url)
+
+
+if __name__ == '__main__':
+    app.run()
