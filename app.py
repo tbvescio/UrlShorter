8cc97620a6d550e640dadd35a87a0932a731ac1e
Tomas <xxxxx@xxxx.com>
2019-05-29 13:05:42 -0300

    hash

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
 import pymysql
 from flask import Flask, request, render_template, redirect
+import hashlib
 
+db = pymysql.connect("tbvescio.mysql.pythonanywhere-services.com", "tbvescio", "15101989python", "tbvescio$url")
+#db = pymysql.connect("localhost", "root", "15101989", "url")
 
-db = pymysql.connect("arishkage.mysql.pythonanywhere-services.com", "arishkage", "15101989python", "arishkage$url")
+cursor = db.cursor() #pone el cursor en la DB
 
-#pone el cursor en la DB
-cursor = db.cursor()
+base_url = 'http://tbvescio.pythonanywhere.com/'
 
-base_url = 'http://arishkage.pythonanywhere.com/'
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.method == 'POST':
-        url = request.form['url']
+        url = request.form['url'] #recive la url
 
-        sql = "INSERT INTO url(ID,URL) VALUES (ID, '{}')".format( url)
+        hash_url = hashlib.sha1(url.encode("UTF-8")).hexdigest() #convierte url a hash
+        hash_url = hash_url[:7] #usa solo 7 chr del hash
+
+        sql = "INSERT INTO url(ID,URL,HASH) VALUES (ID, '{}','{}')".format(url,hash_url)
         cursor.execute(sql)
         db.commit()
 
-        short_url = cursor.lastrowid
-        return render_template('newindex.html', short_url=base_url + str(short_url))
+
+        return render_template('newindex.html', short_url=base_url + str(hash_url))
     return render_template('newindex.html')
 
 @app.route('/<short_url>')
 def redirect_url(short_url):
-    sql = "select url from url where id = {}".format(short_url)
+
+    sql = "select url from url where HASH = '{}'".format(short_url)
     cursor.execute(sql)
-    url = cursor.fetchone()[0]
+    url = cursor.fetchone()[0] #guarda el resultado
+    
 
     return redirect(url)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
